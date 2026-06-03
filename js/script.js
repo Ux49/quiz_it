d9407fb9aa2b594670247050ea5f24cfdaefd36b
Arjun Khanal <ux.arkhanal@gmail.com>
2026-06-03 18:16:11 +0545

    Const decleration and style.css final update

---
+++ js/script.js
@@ -0,0 +1,16 @@
+            const startScreen = document.getElementById('start-screen');
+            const quizScreen = document.getElementById('quiz-screen');
+            const resultScreen = document.getElementById('result-screen');
+
+            const startBtn = document.getElementById('start-btn');
+            const restartBtn = document.getElementById('restart-btn');
+
+            const questionText = document.getElementById('question-text');
+            const answerContainer = document.getElementById('answer-container');
+            const currentQuestionSpan = document.getElementById('current-question');
+            const questionLengthSpan = document.getElementById('question-length');
+            const scoreSpan = document.getElementById('score');
+            const progressBar = document.getElementById('progress');
+            const finalScoreSpan = document.getElementById('final-score');
+            const maxScoreSpan = document.getElementById('max-score');
+            const resultMessage = document.getElementById('result-message');
